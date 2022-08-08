0d5ae516c4ce4c8fe93dc422285d1eb5d0317f88
Randy Blea <blead@us.ibm.com>
2022-08-08 10:26:33 -0500

    Try to get CI tests to work...one more try
    Signed-off-by: Randy Blea <blead@us.ibm.com>

--- plugins/doc_fragments/csm_client_fragment.py
+++ plugins/doc_fragments/csm_client_fragment.py
@@ -11,32 +11,32 @@ __metaclass__ = type
 class ModuleDocFragment(object):
     # Parameters for IBM CSM modules
     DOCUMENTATION = r'''
-options: 
-  hostname: 
-    description: 
-      - The hostname or IP address of the CSM Server.
-    required: true
-    type: str
-  username: 
-    description: 
-      - The username for the CSM server.
-    required: true
-    type: str
-  password: 
-    description:
-      - The password for the username on the CSM server.
-    required: true
-    type: str
-  port: 
-    description: 
-      - The port number for the connection to the CSM server.
-    type: int
-    default: 9559
-  call_properties
-    description: 
-      - List of changeable options when creating a connection to the cSM server.
-    type: dict
-requirements: 
-  - pyCSM >= 1.0.0
-  - python >= 3.6
-'''
+    options: 
+      hostname: 
+        description: 
+          - The hostname or IP address of the CSM Server.
+        required: true
+        type: str
+      username: 
+        description: 
+          - The username for the CSM server.
+        required: true
+        type: str
+      password: 
+        description:
+          - The password for the username on the CSM server.
+        required: true
+        type: str
+      port: 
+        description: 
+          - The port number for the connection to the CSM server.
+        type: int
+        default: 9559
+      call_properties
+        description: 
+          - List of changeable options when creating a connection to the cSM server.
+        type: dict
+    requirements: 
+      - pyCSM >= 1.0.0
+      - python >= 3.6
+    '''
