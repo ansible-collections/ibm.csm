260ec7b12364c0327de2629f1819b1f212449b82
Randy Blea <blead@us.ibm.com>
2022-08-09 17:52:25 -0500

    Support for enable,disable,etc
    Signed-off-by: Randy Blea <blead@us.ibm.com>

--- plugins/modules/ibm_csm_scheduled_task_action.py
+++ plugins/modules/ibm_csm_scheduled_task_action.py
@@ -20,10 +20,21 @@ options:
   id:
     description:
       - The id for the task to issue the action against
+    required: true
     type: str
   action:
     description:
       - The action to run against the scheduled task  ('run', 'enable', 'disable')
+    required: true
+    type: str
+  synchronous:
+    description:
+      - Valid when action is 'run'.  If True, won't return from call until task has completed the run.
+    type: bool
+    default: False
+  at_time:
+    description:
+      - Specify this value to run the task at a certain time.  Leave off to run now. Format of yyyy-MM-dd'T'HH-mm
     type: str
 notes:
   - Supports C(check_mode).
@@ -31,13 +42,56 @@ extends_documentation_fragment: ibm.csm.csm_client_fragment.documentation
 '''
 
 EXAMPLES = r'''
-- name: Run the backup task immediately
+- name: Run the task immediately
   ibm.csm.ibm_csm_scheduled_task_action:
     hostname: "{{ csm_host }}"
     username: "{{ csm_username }}"
     password: "{{ csm_password }}"
     id: scheduled_task_id
     action: 'run'
+    
+- name: Run the task immediately and do not return until complete
+  ibm.csm.ibm_csm_scheduled_task_action:
+    hostname: "{{ csm_host }}"
+    username: "{{ csm_username }}"
+    password: "{{ csm_password }}"
+    id: scheduled_task_id
+    action: 'run'
+    synchronous: True
+
+- name: Run the task at the specified time
+  ibm.csm.ibm_csm_scheduled_task_action:
+    hostname: "{{ csm_host }}"
+    username: "{{ csm_username }}"
+    password: "{{ csm_password }}"
+    id: scheduled_task_id
+    action: 'run'
+    at_time: '2022-08-09T17-15'
+    
+- name: Enable the task now
+  ibm.csm.ibm_csm_scheduled_task_action:
+    hostname: "{{ csm_host }}"
+    username: "{{ csm_username }}"
+    password: "{{ csm_password }}"
+    id: 2
+    action: 'enable'
+    
+- name: Enable the task at the specified time
+  ibm.csm.ibm_csm_scheduled_task_action:
+    hostname: "{{ csm_host }}"
+    username: "{{ csm_username }}"
+    password: "{{ csm_password }}"
+    id: 2
+    action: 'enable'
+    at_time: '2022-08-09T17-30'
+    
+- name: Disable the task 
+  ibm.csm.ibm_csm_scheduled_task_action:
+    hostname: "{{ csm_host }}"
+    username: "{{ csm_username }}"
+    password: "{{ csm_password }}"
+    id: 2
+    action: 'disable'
 '''
 
 RETURN = r''' # '''
@@ -47,14 +101,43 @@ from ansible_collections.ibm.csm.plugins.module_utils.ibm_csm_client import CSMC
 
 
 class ScheduledTaskManager(CSMClientBase):
-    def run_action_now(self):
-        if self.params['action'] == 'run':
+    def _run_task_now(self):
+        if self.params['synchronous']:
+            return self.session_client.run_scheduled_task(self.params['id'], True)
+        else:
             return self.session_client.run_scheduled_task(self.params['id'], False)
+
+    def _run_task_at_time(self):
+        return self.session_client.run_scheduled_task_at_time(self.params['id'], self.params['at_time'])
+
+    def _enable_task(self):
+        return self.session_client.enable_scheduled_task(self.params['id'])
+
+    def _enable_task_at_time(self):
+        return self.session_client.enable_scheduled_task_at_time(self.params['id'], self.params['at_time'])
+
+    def _disable_task(self):
+        return self.session_client.disable_scheduled_task(self.params['id'])
+
+    def perform_task_action(self):
+        if self.params['action'] == 'run':
+            if self.params['at_time'] is None:
+                return self._run_task_now()
+            else:
+                return self._run_task_at_time()
+        if self.params['action'] == 'enable':
+            if self.params['at_time'] is None:
+                return self._enable_task()
+            else:
+                return self._enable_task_at_time()
+        if self.params['action'] == 'disable':
+            return self._disable_task()
 
 
 def main():
     argument_spec = csm_argument_spec()
-    argument_spec.update(id=dict(type='str'), action=dict(type='str'))
+    argument_spec.update(id=dict(type='str'), action=dict(type='str'),
+                         synchronous=dict(type='bool'), at_time=dict(type='str'))
 
     module = AnsibleModule(
         argument_spec=argument_spec,
@@ -63,7 +146,7 @@ def main():
 
     scheduled_task_manager = ScheduledTaskManager(module)
 
-    result = scheduled_task_manager.run_action_now()
+    result = scheduled_task_manager.perform_task_action()
 
     module.exit_json(changed=scheduled_task_manager.changed, result=result.json())
 
